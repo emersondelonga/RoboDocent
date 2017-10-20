0702419ed91a546da9d43d14692b0dedb463126a
emersondelonga <emersondelonga@gmail.com>
2017-10-20 15:08:17 -0400

    Updated a bit. Maybe fixed

--- us.py
+++ us.py
@@ -2,7 +2,8 @@
 
 import pigpio, eyw2
 import numpy as np
-
+trig = 20
+echo = 21
 class US:
     """ A class for an ultrasonic sensor."""
     def __init__(self, trig, echo):
@@ -10,10 +11,10 @@ class US:
         self.echo = echo
         self.readings = [128, 128, 128, 128, 128]
 
-    def get_distance(): 
+    def get_distance(self): 
 
         currentReading =  eyw2.read_ultrasonic_ranger(trig, echo)
-        if currentReading != null:
+        if currentReading != none:
             readings.pop(0)
             readings.append(currentReading)
 
